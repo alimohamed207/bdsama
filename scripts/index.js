148a6f21a1f48c707126887ddf8603c7e01b9411
Ali Mohamed <106467889+alimohamed207@users.noreply.github.com>
2022-11-23 17:20:04 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Nov 15, 2022',
+  birthdate: 'Nov 15, 2023',
   name: 'Sama'
 };
 
